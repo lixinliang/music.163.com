3909958ab02bba5ca307fca01b1d1fbc9953bf31
李昕亮 <lixinliang@yy.com>
2017-01-14 17:44:48 +0800

    update source
    update source

--- src/bubble.js
+++ src/bubble.js
@@ -18,11 +18,6 @@ let random = {
     },
 };
 
-// let stage;
-// const R = Math.pow(16, 5);
-// const G = Math.pow(16, 3);
-// const B = Math.pow(16, 1);
-
 export default {
     init () {
         this.stage = new BubbleHearts();
@@ -31,16 +26,20 @@ export default {
     animate () {
         if (resource.length) {
             let image = resource[random.uniformDiscrete(0, resource.length - 1)];
+            /**
+             * Create mask
+             */
             let canvas = document.createElement('canvas');
             let context = canvas.getContext('2d');
             canvas.width = image.width / 2;
             canvas.height = image.height / 2;
             context.drawImage(image, 0, 0, image.width / 2, image.height / 2);
             context.globalCompositeOperation = 'source-in';
-            // context.fillStyle = `#${ (0xc70c0c + random.uniformDiscrete(0, 3) * R + random.uniformDiscrete(0, 3) * G + random.uniformDiscrete(0, 3) * B ).toString(16) }`;
-            // rgb(245, 77, 77)
             context.fillStyle = `#${ (245).toString(16) }${ (77).toString(16) }${ (77).toString(16) }`;
             context.fillRect(0, 0, canvas.width, canvas.height);
+            /**
+             * Animate
+             */
             this.stage.bubble(canvas);
         }
     },
