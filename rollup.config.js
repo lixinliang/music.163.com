7f9560ee4962c0a87e388b370d2683e7ef3314b2
李昕亮 <lixinliang@yy.com>
2017-01-16 09:34:40 +0800

    update rollup
    update rollup

--- rollup.config.js
+++ rollup.config.js
@@ -5,6 +5,8 @@ import babel from 'rollup-plugin-babel';
 import image from 'rollup-plugin-image';
 import uglify from 'rollup-plugin-uglify';
 import bookmark from 'rollup-plugin-bookmark';
+import commonjs from 'rollup-plugin-commonjs';
+import nodeResolve from 'rollup-plugin-node-resolve';
 
 export default {
     format : 'iife',
@@ -16,5 +18,12 @@ export default {
         image(),
         uglify(),
         bookmark(),
+        commonjs({
+            sourceMap : false,
+        }),
+        nodeResolve({
+            jsnext : true,
+            main : true,
+        }),
     ],
 };
